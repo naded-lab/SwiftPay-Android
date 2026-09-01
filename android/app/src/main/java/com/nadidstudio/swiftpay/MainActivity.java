690440a1b58940742df7a5c68334ad7d3a1dcefe
Ni <nadedashraf@gmail.com>
2026-09-01 17:46:45 +0300

    ci: build stage 3 apk with github actions

--- android/app/src/main/java/com/nadidstudio/swiftpay/MainActivity.java
+++ android/app/src/main/java/com/nadidstudio/swiftpay/MainActivity.java
@@ -1,5 +1,12 @@
 package com.nadidstudio.swiftpay;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(UssdPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
